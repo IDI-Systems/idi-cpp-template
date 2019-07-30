93afd2305fd2dfbddb380310b1f04b85e7b51ded
Nou <cliff.foster@idi-systems.com>
2019-07-30 14:48:00 -0700

    Catch2 init

---
+++ src/app/components.cmake
@@ -0,0 +1,12 @@
+#
+# @author Cliff Foster (Nou) <cliff@idi-systems.com>
+#
+# @copyright Copyright (c) 2019 International Development & Integration Systems LLC
+#
+
+###############################################################################
+# ADD COMPONENT FOLDERS BELOW                                                 #
+# !!! DO NOT ADD THE main FOLDER AS A COMPONENT !!!                           #
+###############################################################################
+
+add_subdirectory("common")
