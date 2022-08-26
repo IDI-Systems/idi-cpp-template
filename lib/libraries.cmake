96f8c8086dc1f3447954deedb3f9074aad1ac59d
Cliff Foster <cliff.foster@idi-systems.com>
2022-08-26 10:48:01 -0700

    Changed libraries.cmake to be a better example

--- lib/libraries.cmake
+++ lib/libraries.cmake
@@ -1,4 +1,4 @@
 # Include libraries here.
 # Catch2 is included by deault, no need to re-add it here.
 
-#add_subdirectory(${IDI_EXTERNAL_LIB_DIR}/cmake-cpp-inner)
+# add_subdirectory(${IDI_EXTERNAL_LIB_DIR}/lib_folder)
