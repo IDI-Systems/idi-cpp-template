869116511802168358345d036f944cfc1b965261
Cliff Foster <cliff.foster@idi-systems.com>
2023-04-07 10:57:14 -0700

    WIP

--- cmake/idi/updater/updater_imp.cmake
+++ cmake/idi/updater/updater_imp.cmake
@@ -2,5 +2,8 @@
 
 macro(do_replace)
     file(REMOVE_RECURSE ${ARGV0})
-    file(COPY ${ARGV1})
+    file(COPY ${ARGV1} DESTINATION ${ARGV0})
 endmacro()
+
+do_replace("${PROJECT_SOURCE_DIR}/cmake/idi/" "${FRAMEWORK_UPDATE_DIR}/cmake/idi/")
+
