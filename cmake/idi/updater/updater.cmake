c65866ba4053f2ab8bae8229d4f6621e9ee4f0fc
Cliff Foster <cliff.foster@idi-systems.com>
2023-04-08 09:29:32 -0700

    Add file and folder and url modes properly. Git can wait because its annoying.

--- cmake/idi/updater/updater.cmake
+++ cmake/idi/updater/updater.cmake
@@ -1,5 +1,7 @@
 # TODO: remove this set after developing
-set(FRAMEWORK_UPDATE_FILE_LOC ${PROJECT_SOURCE_DIR})
+include(FetchContent)
+
+set(FRAMEWORK_UPDATE_FOLDER_LOC ${PROJECT_SOURCE_DIR})
 
 macro(move_var)
     set(${ARGV1} ${${ARGV0}})
@@ -7,7 +9,7 @@ macro(move_var)
 endmacro()
 
 macro(do_error)
-    file(REMOVE_RECURSE ${__framework_update_dir})
+    #file(REMOVE_RECURSE ${FRAMEWORK_UPDATE_DIR})
     message(FATAL_ERROR ${ARGN})
 endmacro()
 
@@ -19,7 +21,9 @@ if(DO_FRAMEWORK_UPDATE)
 
     move_var(FRAMEWORK_UPDATE_MODE __framework_update_mode)
     move_var(FRAMEWORK_UPDATE_FILE_LOC __framework_update_file_loc)
+    move_var(FRAMEWORK_UPDATE_FOLDER_LOC __framework_update_folder_loc)
     move_var(FRAMEWORK_UPDATE_FORCE __framework_update_force)
+    move_var(FRAMEWORK_UPDATE_URL __framework_update_url)
 
     message(STATUS "Updating IDI CMake Framework!")
 
@@ -30,23 +34,32 @@ if(DO_FRAMEWORK_UPDATE)
 
 
     if(NOT __framework_update_mode)
-        set(__framework_update_mode "file")
+        set(__framework_update_mode "folder")
     endif()
 
     message(STATUS "Framework update mode: ${__framework_update_mode}")
 
-    if(__framework_update_mode STREQUAL "file")
-        if (NOT __framework_update_file_loc)
-            do_error("FRAMEWORK_UPDATE_MODE is 'file', but FRAMEWORK_UPDATE_FILE_LOC is not defined!")
+    if(__framework_update_mode STREQUAL "folder")
+        if (NOT __framework_update_folder_loc)
+            do_error("FRAMEWORK_UPDATE_MODE is 'folder', but FRAMEWORK_UPDATE_FOLDER_LOC is not defined!")
         endif()
 
-        file(COPY "${__framework_update_file_loc}/" DESTINATION ${FRAMEWORK_UPDATE_DIR}
+        file(COPY "${__framework_update_folder_loc}/" DESTINATION ${FRAMEWORK_UPDATE_DIR}
             PATTERN ".idi-framework-update" EXCLUDE
             PATTERN "build" EXCLUDE
             PATTERN ".git" EXCLUDE
             )
+    elseif(__framework_update_mode STREQUAL "file")
+        file(ARCHIVE_EXTRACT INPUT "${PROJECT_SOURCE_DIR}/${__framework_update_file_loc}" DESTINATION ${FRAMEWORK_UPDATE_DIR})
+        if(NOT (EXISTS "${FRAMEWORK_UPDATE_DIR}/cmake/idi/updater/updater_version.cmake"))
+            do_error("The downloaded file provided via FRAMEWORK_UPDATE_URL does not appear to be a valid template update!")
+        endif()
     elseif(__framework_update_mode STREQUAL "url")
-        do_error("FRAMEWORK_UPDATE_MODE is 'url' but it is not implemented yet!")
+        file(DOWNLOAD ${__framework_update_url} "${PROJECT_SOURCE_DIR}/.idi_framework_dl")
+        file(ARCHIVE_EXTRACT INPUT "${PROJECT_SOURCE_DIR}/.idi_framework_dl" DESTINATION ${FRAMEWORK_UPDATE_DIR})
+        if(NOT (EXISTS "${FRAMEWORK_UPDATE_DIR}/cmake/idi/updater/updater_version.cmake"))
+            do_error("The downloaded file provided via FRAMEWORK_UPDATE_URL does not appear to be a valid template update!")
+        endif()
     elseif(__framework_update_mode STREQUAL "git")
         do_error("FRAMEWORK_UPDATE_MODE is 'git' but it is not implemented yet!")
     else()
@@ -62,7 +75,6 @@ if(DO_FRAMEWORK_UPDATE)
     set(OLD_IDI_BASE_REQ_CML_V ${IDI_BASE_REQ_CML_V})
 
     include("${FRAMEWORK_UPDATE_DIR}/cmake/idi/updater/updater_version.cmake")
-    include("${FRAMEWORK_UPDATE_DIR}/cmake/idi/updater/updater_version.cmake")
 
     if ((OLD_IDI_CPP_UPDATER_VERSION EQUAL IDI_CPP_UPDATER_VERSION) AND
         (OLD_IDI_CPP_FRAMEWORK_VERSION_MAJOR EQUAL IDI_CPP_FRAMEWORK_VERSION_MAJOR) AND
@@ -77,13 +89,13 @@ if(DO_FRAMEWORK_UPDATE)
         endif()
     endif()
 
-    if(NOT OLD_IDI_CPP_UPDATER_VERSION EQUAL IDI_CPP_UPDATER_VERSION)
-        include("${__framework_update_dir}/cmake/idi/updater/updater_imp.cmake")
-    else()
-        include("${PROJECT_SOURCE_DIR}/cmake/idi/updater/updater_imp.cmake")
-    endif()
+    # if(NOT OLD_IDI_CPP_UPDATER_VERSION EQUAL IDI_CPP_UPDATER_VERSION)
+    #     include("${FRAMEWORK_UPDATE_DIR}/cmake/idi/updater/updater_imp.cmake")
+    # else()
+    #     include("${PROJECT_SOURCE_DIR}/cmake/idi/updater/updater_imp.cmake")
+    # endif()
 
-    file(REMOVE_RECURSE ${FRAMEWORK_UPDATE_DIR})
+    # file(REMOVE_RECURSE ${FRAMEWORK_UPDATE_DIR})
 endif()
 
 
