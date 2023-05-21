feff8715c308eae77905e284d0bfba59b6cb31c6
Cliff Foster <cliff.foster@idi-systems.com>
2023-05-21 12:40:42 -0700

    Adding a commitable optional file as well.

---
+++ platform-options.cmake
@@ -0,0 +1,3 @@
+# This is an optional file to add set functions for platform specific variables.
+# These variables should be prefixed with the project name or otherwise unique and
+# not prone to collision in other libraries.
