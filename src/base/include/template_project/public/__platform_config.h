27f4fc03ee37ed2ef6edcc3b01405b47114e487d
Timi Ornik <timi@idi-systems.com>
2024-02-12 16:28:51 +0100

    Copyright 2024

--- src/base/include/template_project/public/__platform_config.h
+++ src/base/include/template_project/public/__platform_config.h
@@ -3,7 +3,7 @@
  *
  * Defines for getting the platform configuration of the project
  *
- * @copyright Copyright (c) 2023 International Development & Integration Systems LLC
+ * @copyright Copyright (c) 2024 International Development & Integration Systems LLC
  *
  */
 // NOLINTBEGIN
