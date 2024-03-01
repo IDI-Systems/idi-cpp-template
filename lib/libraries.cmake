568726414b3716b52ea92b69c0ea55866c8c0ba3
Cliff Foster <cliff@idi-systems.com>
2024-02-29 19:57:13 -0800

    Clean up a bit

--- lib/libraries.cmake
+++ lib/libraries.cmake
@@ -1,5 +1,2 @@
 # Include libraries here.
-# Catch2 is included by deault, no need to re-add it here.
-
-# idi_add_third_party_dependency(Catch2 https://github.com/catchorg/Catch2.git v3.5.2)
-
+# Catch2 is included by default, no need to re-add it here.
