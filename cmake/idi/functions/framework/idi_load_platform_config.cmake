577c70b3d92434fe644e7402f3a9b6e1142951d2
Timi Ornik <timi@idi-systems.com>
2025-07-22 13:22:32 +0200

    Fix IDICMAKE_PLATFORM_CONFIG, Add GitLab Windows CI and simplify platform config selection in CI

--- cmake/idi/functions/framework/idi_load_platform_config.cmake
+++ cmake/idi/functions/framework/idi_load_platform_config.cmake
@@ -11,7 +11,7 @@ macro(idi_load_platform_config)
     idi_cmake_hook_abs(${CMAKE_CURRENT_LIST_DIR}/platform-local-options.cmake)
     idi_cmake_hook_abs(${CMAKE_CURRENT_LIST_DIR}/cmake-hooks/pre-configure.cmake)
 
-    set(IDICMAKE_PLATFORM_CONFIG "${CMAKE_CURRENT_LIST_DIR}/platform-config.cmake")
+    set(IDICMAKE_PLATFORM_CONFIG "${CMAKE_CURRENT_LIST_DIR}/platform-config.cmake" CACHE FILEPATH "Platform config definitions file")
     include("${IDICMAKE_PLATFORM_CONFIG}")
 
     idi_cmake_hook_abs(${CMAKE_CURRENT_LIST_DIR}/cmake-hooks/post-platform-config.cmake)
